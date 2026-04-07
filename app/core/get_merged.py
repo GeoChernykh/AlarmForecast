b4f704acc794bacde7b3bd3331489d27436c1e6e
Heorhii <geochernykh@gmail.com>
2026-04-07 10:01:54 +0300

    comments

--- app/core/get_merged.py
+++ app/core/get_merged.py
@@ -4,7 +4,8 @@ from app.db.database import Database
 
 def get_merged(dp_path, start_date) -> pd.DataFrame:
     with Database(dp_path) as db:
-        isw = db.isw.get(start_date=start_date)
+        # returns rows from database. You need to wrap them into pandas DataFrame
+        isw_rows = db.isw.get(start_date=start_date)
         # alarms
         # weather
         # telegram
